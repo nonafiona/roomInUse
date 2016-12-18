9ae1333fdd09a655b97ebdd56e0978ce4a696a66
fiona <fi.carty@gmail.com>
2016-12-17 20:53:51 -0800

    Initial Commit

---
+++ roomInUse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  roomInUse
+//
+//  Created by Fiona Carty on 12/17/16.
+//  Copyright © 2016 Make School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
